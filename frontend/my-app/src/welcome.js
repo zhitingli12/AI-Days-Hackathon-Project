ede5ff86d8218ddd503e145b050f47c98aa7b1cf
zhitingli12 <zhitingli54678@gmail.com>
2024-10-26 13:53:50 -0400

    welcome screen
    i got the code but i need it to start running

---
+++ frontend/my-app/src/welcome.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './welcome.css';
+
+export const Iphone = () => {
+    return (
+        <div className="iphone">
+            <div className="image-wrapper">
+                <img
+                    className="image"
+                    alt="Image"
+                    src="https://c.animaapp.com/0G8AOMXl/img/image-1@2x.png"
+                />
+            </div>
+        </div>
+    );
+};
