0e45536908af2cdb690f87715f8aecc45b175694
zhitingli12 <zhitingli54678@gmail.com>
2024-10-27 13:02:42 -0400

    add back welcome

--- frontend/my-app/my-expo/components/screens/welcome.tsx
+++ frontend/my-app/my-expo/components/screens/welcome.tsx
@@ -71,7 +71,7 @@ export const Iphone = (): JSX.Element => {
           <View style={styles.imageWrapper}>
             <Image
               style={styles.image}
-              source={{ uri: "https://i.imgur.com/J1Qy3ah.png" }}
+              source={{ uri: "https://c.animaapp.com/6ThwplpN/img/image-1@2x.png" }}
             />
           </View>
           {location ? (
@@ -114,8 +114,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   image: {
-    width: 100, // Adjust width as needed
-    height: 150, // Adjust height as needed
+    width: 247, 
+    height: 277,
     marginBottom: 20,
     resizeMode: "contain",
   },
